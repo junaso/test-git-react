65f0141ab40982dfc5435dc9861b0ac7c6392527
NAM JUN <namj@ais-info.co.jp>
2023-10-27 09:31:59 +0900

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          test yester
+          test yesterday
         </a>
       </header>
     </div>
